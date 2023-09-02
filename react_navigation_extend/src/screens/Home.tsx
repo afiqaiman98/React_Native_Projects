8b00259cd672f6eff25a303ee0a3b40f550f0dc8
afiq aiman <afiqaiman091@gmail.com>
2023-09-02 11:58:25 +0800

    navigation

---
+++ react_navigation_extend/src/screens/Home.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, FlatList, Pressable } from "react-native";
+import React from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackPramList } from "../../App";
+import { PRODUCTS_LIST } from "../data/constants";
+import Seperator from "../components/Seperator";
+import ProductItem from "../components/ProductItem";
+
+type HomeProps = NativeStackScreenProps<RootStackPramList, "Home">;
+
+const Home = ({ navigation }: HomeProps) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={PRODUCTS_LIST}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={Seperator}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => {
+              navigation.navigate("Details", { product: item });
+            }}
+          >
+            <ProductItem product={item} />
+          </Pressable>
+        )}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "center",
+
+    padding: 12,
+    backgroundColor: "#FFFFFF",
+  },
+});
+
+export default Home;
